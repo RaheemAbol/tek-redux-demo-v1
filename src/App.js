8647735dc2cd248dd2ad388ffad6e2a0ccdc1ce4
Raheem Abolfathzadeh <raheemabol91@gmail.com>
2023-07-21 13:01:19 -0400

    *

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { useSelector } from "react-redux";
+import "./App.css";
+import Login from "./components/Login";
+import NavBar from "./components/NavBar";
+import Profile from "./components/Profile";
 
 function App() {
+  const themeColor = useSelector((state) => state.theme.value);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={themeColor}>
+      <NavBar />
+      <Profile />
+      <Login />
     </div>
   );
 }
